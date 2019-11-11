451ac8c1379d7e9bc3e19283e5b2630a61465f64
pelogvc <pelogvc@gmail.com>
2019-11-12 03:59:09 +0900

    Add MutationObserver

--- src/content/netflix.ts
+++ src/content/netflix.ts
@@ -1,1 +1,99 @@
-console.log('content/netflix')
+import { browser, Runtime } from "webextension-polyfill-ts";
+
+class Netflix {
+    private obInterval: number = 500;
+    private ob: MutationObserver | undefined;
+    private obEl: Element | null | undefined;
+    private obOptions = {
+        attributes: true,
+        attributeFilter: ["class"],
+        childList: true,
+        characterData: false,
+        subtree: true,
+        attributeOldValue: false
+    };
+    private obTimer: any;
+    private bgPort: Runtime.Port;
+
+    constructor(options: MutationObserverInit = {}) {
+        this.obOptions = {
+            ...this.obOptions,
+            ...options
+        };
+
+        // connect background
+        this.bgPort = browser.runtime.connect(undefined, {
+            name: "netflix"
+        });
+
+        this.bgPort.onMessage.addListener(this.receiveMessage.bind(this));
+    }
+
+    private receiveMessage(
+        message: { id: string; data: Object },
+        port: Runtime.Port
+    ) {
+        console.log(message);
+    }
+
+    public applyObserver() {
+        if (this.obTimer) return clearTimeout(this.obTimer);
+        if (!this.proccessApplyObserver()) {
+            this.obTimer = setTimeout(
+                this.applyObserver.bind(this),
+                this.obInterval
+            );
+        }
+    }
+
+    private isMovie(duration: string): boolean {
+        return !!(duration.includes("시간") || duration.includes("분"));
+    }
+
+    private proccessObserverManager(mutationRecords: MutationRecord[]) {
+        mutationRecords.forEach(async mutationRecord => {
+            const target = mutationRecord.target as Element;
+
+            if (
+                target.classList.contains("jawBoneOpenContainer") || // jawBoneOpenContainer jqwBoneOpen-enter
+                target.classList.contains("jawBoneContainer")
+                //target.classList.contains("jawBoneFadeInPlace-enter-active") // jawBoneFadeInPlaceContainer jawBoneFadeInPlace-enter
+            ) {
+                const title =
+                    target.querySelector(".logo") &&
+                    target.querySelector(".logo")!.getAttribute("alt");
+                const year =
+                    target.querySelector(".year") &&
+                    target.querySelector(".year")!.textContent;
+                const duration =
+                    target.querySelector(".duration") &&
+                    target.querySelector(".duration")!.textContent;
+
+                if (!title || !year || !duration) return;
+                console.log(title, year, duration, this.isMovie(duration));
+            }
+        });
+    }
+
+    private proccessApplyObserver() {
+        //this.obEl = document.querySelector(this.obTarget as unknown as string)
+        this.obEl = document.querySelector(".mainView");
+
+        if (!this.obEl) return false;
+
+        this.ob = new MutationObserver(this.proccessObserverManager.bind(this));
+
+        this.ob.observe(this.obEl, this.obOptions);
+
+        console.log(`apply observer: ${this.obEl}`);
+
+        return true;
+    }
+}
+
+//console.log('content/netflix')
+
+(() => {
+    const netflix = new Netflix();
+    netflix.applyObserver();
+})();
--- webpack.config.js
+++ webpack.config.js
@@ -6,63 +6,63 @@ const CleanWebpackPlugin = require("clean-webpack-plugin");
 
 const mode = process.env.NODE_ENV;
 module.exports = {
-  mode,
-  devtool: "inline-source-map",
-  entry: {
-    "content-script": "./src/content/netflix.ts",
-    background: "./src/background/index.ts"
-    //popup: "./src/popup/popup.ts"
-  },
-  output: {
-    publicPath: ".",
-    path: resolve(__dirname, "dist/"),
-    filename: "[name].js",
-    libraryTarget: "umd"
-  },
-  plugins: [
-    new ExtensionReloaderPlugin({
-      port: 9090, // Which port use to create the server
-      reloadPage: true, // Force the reload of the page also
-      entries: {
-        contentScript: "content-script",
-        background: "background",
-        extensionPage: "popup"
-      }
-    }),
+    mode,
+    devtool: "inline-source-map",
+    entry: {
+        "content-script": "./src/content/netflix.ts",
+        background: "./src/background/index.ts"
+        //popup: "./src/popup/popup.ts"
+    },
+    output: {
+        publicPath: ".",
+        path: resolve(__dirname, "dist/"),
+        filename: "[name].js",
+        libraryTarget: "umd"
+    },
+    plugins: [
+        new ExtensionReloaderPlugin({
+            port: 9090, // Which port use to create the server
+            reloadPage: true, // Force the reload of the page also
+            entries: {
+                contentScript: "content-script",
+                background: "background",
+                extensionPage: "popup"
+            }
+        }),
 
-    new MiniCssExtractPlugin({ filename: "style.css" }),
-    new CopyWebpackPlugin([
-      {
-        from:
-          process.env.NODE_ENV === "development"
-            ? "./src/manifest.json"
-            : "./src/manifest.json",
-        to: "manifest.json"
-      },
-      { from: "./src/popup/popup.html" },
-      { from: "./src/icons" }
-    ])
-  ],
-  module: {
-    rules: [
-      {
-        test: /\.tsx?$/,
-        use: "ts-loader",
-        exclude: /node_modules/
-      },
-      {
-        test: /\.css$/,
-        use: [
-          {
-            loader: MiniCssExtractPlugin.loader
-          },
-          "css-loader"
+        new MiniCssExtractPlugin({ filename: "style.css" }),
+        new CopyWebpackPlugin([
+            {
+                from:
+                    process.env.NODE_ENV === "development"
+                        ? "./src/manifest.json"
+                        : "./src/manifest.json",
+                to: "manifest.json"
+            },
+            { from: "./src/popup/popup.html" },
+            { from: "./src/icons" }
+        ])
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                use: "ts-loader",
+                exclude: /node_modules/
+            },
+            {
+                test: /\.css$/,
+                use: [
+                    {
+                        loader: MiniCssExtractPlugin.loader
+                    },
+                    "css-loader"
+                ]
+            },
+            {
+                test: /\.txt$/,
+                use: "raw-loader"
+            }
         ]
-      },
-      {
-        test: /\.txt$/,
-        use: "raw-loader"
-      }
-    ]
-  }
+    }
 };
